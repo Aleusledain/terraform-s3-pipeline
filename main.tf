659573a5a48fd8ea5bf6c42920791a7bf12ba2f8
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-08-01 19:48:50 -0400

    main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_first_bucket" {
+  bucket = "my-first-real-bucket-1"  # Replace with your own unique name!
+  acl    = "private"
+
+  tags = {
+    Name        = "TestBucket"
+    Environment = "Dev"
+  }
+}
